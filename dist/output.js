1cccebdebab9d3dfbf56f8c2911396b7d84cdc30
subkh4n <subkh4n@gmail.com>
2023-03-15 11:10:46 +0700

    ok

--- dist/output.js
+++ dist/output.js
@@ -1,56 +1,56 @@
 
-function playaudio() {
-  return {
-    // State variables for determining if audio is playing or not
-    currentlyPlaying: false,
-    status: '',
-
-    // Toggle the currentlyPlaying variable when user clicks Play/Stop button
-    async playAndStop() {
-      const audio = this.$refs.audio;
-
-      // If audio is playing, pause it and set state accordingly
-      if (this.currentlyPlaying) {
-        try {
-          await audio.pause();
-          this.currentlyPlaying = false;
-          this.status = 'Audio Stopped.';
-        } catch (error) {
-          console.error(error);
-        }
-      }
-      // Else, start playing audio and set state accordingly
-      else {
-        try {
-          await audio.play();
-          this.currentlyPlaying = true;
-          this.status = 'Audio Playing...';
-        } catch (error) {
-          console.error(error);
-        }
-      }
-    }
-  };
-}
-
-
-
 // function playaudio() {
 //   return {
+//     // State variables for determining if audio is playing or not
 //     currentlyPlaying: false,
-//     //play and stop the audio
-//     playAndStop() {
+//     status: '',
+
+//     // Toggle the currentlyPlaying variable when user clicks Play/Stop button
+//     async playAndStop() {
+//       const audio = this.$refs.audio;
+
+//       // If audio is playing, pause it and set state accordingly
 //       if (this.currentlyPlaying) {
-//         this.$refs.audio.pause();
-//         this.$refs.audio.currentTime = 0;
-//         this.currentlyPlaying = false;
-//       } else {
-//         this.$refs.audio.play();
-//         this.currentlyPlaying = true;
+//         try {
+//           await audio.pause();
+//           this.currentlyPlaying = false;
+//           this.status = 'Audio Stopped.';
+//         } catch (error) {
+//           console.error(error);
+//         }
 //       }
-//     },
+//       // Else, start playing audio and set state accordingly
+//       else {
+//         try {
+//           await audio.play();
+//           this.currentlyPlaying = true;
+//           this.status = 'Audio Playing...';
+//         } catch (error) {
+//           console.error(error);
+//         }
+//       }
+//     }
 //   };
 // }
+
+
+
+function playaudio() {
+  return {
+    currentlyPlaying: false,
+    //play and stop the audio
+    playAndStop() {
+      if (this.currentlyPlaying) {
+        this.$refs.audio.pause();
+        this.$refs.audio.currentTime = 0;
+        this.currentlyPlaying = false;
+      } else {
+        this.$refs.audio.play();
+        this.currentlyPlaying = true;
+      }
+    },
+  };
+}
 
 // https://developer.chrome.com/blog/play-request-was-interrupted/
 
